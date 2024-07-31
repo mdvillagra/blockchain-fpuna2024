0226d9ed423bd84cfff3436aaac6eef6a65cc22d
Pablo Cesar Bordon Battilana <pbordon@fpj.com.py>
2024-07-31 14:17:09 -0400

    Segundo Final Pablo Bordon Battilana

---
+++ final/Pablo Bordon Battilana/contracts/token.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+ 
+// Your token contract
+contract Token is Ownable, ERC20 {
+    string private constant _symbol = 'MYT';                 // TODO: Give your token a symbol (all caps!)
+    string private constant _name = 'MyToken';                 // TODO: Give your token a name
+
+    bool private mintingDisabled = false;
+    // TODO: add private members as needed!
+    // TODO: if you create private member, initialize it in the constructor
+    
+    constructor() ERC20(_name, _symbol) {}
+
+    // ============================================================
+    //                    FUNCTIONS TO IMPLEMENT
+    // ============================================================
+
+    // Function mint: Create more of your tokens.
+    // You can change the inputs, or the scope of your function, as needed.
+    // Do not remove the onlyOwner modifier!
+    function mint(uint amount) public onlyOwner {
+        require(!mintingDisabled, "Minting is disabled");
+        _mint(msg.sender, amount);
+    }
+
+    // Function disable_mint: Disable future minting of your token.
+    // You can change the inputs, or the scope of your function, as needed.
+    // Do not remove the onlyOwner modifier!
+    function disable_mint() public onlyOwner {
+        mintingDisabled = true;
+    }
+}
